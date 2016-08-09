b0134d6c934fd2ee3481f2ef116ef8aae3edf2c3
SRK.Lyu <superalsrk@gmail.com>
2016-08-09 21:02:06 +0800

    add github button

--- app/containers/DashboardContainer.js
+++ app/containers/DashboardContainer.js
@@ -24,7 +24,7 @@ class DashboardContainer extends React.Component {
     render() {
         return (
             <div>
-            
+                <Github/>
                 <Header/>
                 <Leftbar/>
                 <div className="sidebar-bg"></div>
